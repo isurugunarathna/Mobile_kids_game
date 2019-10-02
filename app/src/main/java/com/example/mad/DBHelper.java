993612cbf4ab8ab8dfcb4cd0d52ab249ee263b6c
Isuru Pramodya <50676533+isurugunarathna@users.noreply.github.com>
2019-10-02 20:33:30 +0530

    Update helper

--- app/src/main/java/com/example/mad/DBHelper.java
+++ app/src/main/java/com/example/mad/DBHelper.java
@@ -6,6 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Scanner;
